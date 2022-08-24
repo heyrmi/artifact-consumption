b362c0021fe17dec91831d805868e65e87d70810
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-08-24 18:01:58 +0530

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void printMyName(String name) {
+        System.out.println("Hi" + name + ". How have you been?");
+    }
+}
