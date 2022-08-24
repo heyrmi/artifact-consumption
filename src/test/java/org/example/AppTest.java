d12488c10704ac405c4c496f4caf624058cb7099
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-08-24 18:15:05 +0530

    Replaced: JUnit with TestNG

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
